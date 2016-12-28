7dde44a5ad065b41b8e55949b555b92e9977eec7
Cody Beyer <cobey@microsoft.com>
2016-12-28 12:18:42 -0800

    Added HTTPS warning

--- MasterDetail-Forms-Azure/MasterDetail/App.xaml.cs
+++ MasterDetail-Forms-Azure/MasterDetail/App.xaml.cs
@@ -12,6 +12,7 @@ namespace MasterDetail
 {
 	public partial class App : Application
 	{
+        //MUST use HTTPS, neglecting to do so will result in runtime errors on iOS
 		public static bool AzureNeedsSetup => AzureMobileAppUrl == "https://CONFIGURE-THIS-URL.azurewebsites.net";
 		public static string AzureMobileAppUrl = "https://CONFIGURE-THIS-URL.azurewebsites.net";
         public static IDictionary<string, string> LoginParameters => null;
